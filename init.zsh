abc593176137b545a9c61027449a5bbcbdd3f93d
Joke de Buhr <joke@xckk.de>
2023-03-12 16:28:29 +0100

    feat: cache starship initfile
    Signed-off-by: Joke de Buhr <joke@xckk.de>

--- init.zsh
+++ init.zsh
@@ -3,11 +3,19 @@
   local command=${commands[starship]:-"$(${commands[asdf]} which starship 2> /dev/null)"}
   [[ -z $command ]] && return 1
 
+  # generating init file
+  local initfile=$1/starship-init.zsh
+  if [[ ! -e $initfile || $initfile -ot $command ]]; then
+    $command init zsh --print-full-init >| $initfile
+    zcompile -UR $initfile
+  fi
+
+  # generating completions
   local compfile=$1/functions/_starship
   if [[ ! -e $compfile || $compfile -ot $command ]]; then
     $command completions zsh >| $compfile
     zimfw check-dumpfile
   fi
 
-  source <($command init zsh --print-full-init)
+  source $initfile
 } ${0:h}
